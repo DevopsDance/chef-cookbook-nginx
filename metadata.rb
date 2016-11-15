e55944d9e483266a44caacada5b5bdaa6a2bd2ec
Marcin Wolny <marcin@wutanic.com>
2016-11-15 23:16:01 +0100

    New release: 0.1.5
        - Initial integration tests.
        - By default attribute server_tokens options is set to off.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'DevopsDance'
 maintainer_email 'team@devops.dance'
 license 'All rights reserved'
 description 'Installs and configures NGINX'
-version '0.1.4'
+version '0.1.5'
 source_url 'https://github.com/DevopsDance/chef-cookbook-nginx'
 issues_url 'https://github.com/DevopsDance/chef-cookbook-nginx/issues'
 
