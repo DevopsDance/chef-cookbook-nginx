0bd642313b4bd43a738fca3709b35c47511ba92b
Marcin Wolny <marcin@wutanic.com>
2016-11-15 23:13:15 +0100

    Changes in integration tests
        - No need to run tests against several ports.
        - Validate nginx_conf resource.

--- recipes/_tests.rb
+++ recipes/_tests.rb
@@ -3,3 +3,9 @@ include_recipe 'nginx'
 nginx_site 'tests' do
   action :enable
 end
+
+nginx_conf 'multi_accept' do
+  value 'on'
+  context :events
+  action :create
+end
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,19 +1,18 @@
 require 'spec_helper'
 
 describe 'default' do
-  ports = [
-    80,
-    81
-  ]
-  ports.each do |port|
-    describe http_get(port, 'localhost', '/') do
-      its(:status) { should eq 200 }
-      its(:headers) { should include('server' => 'nginx') }
-    end
+  describe http_get(80, 'localhost', '/') do
+    its(:status) { should eq 200 }
+    its(:headers) { should include('server' => 'nginx') }
+  end
 
-    describe http_get(port, 'localhost', '/error') do
-      its(:status) { should eq 404 }
-      its(:headers) { should include('server' => 'nginx') }
-    end
+  describe http_get(80, 'localhost', '/error') do
+    its(:status) { should eq 404 }
+    its(:headers) { should include('server' => 'nginx') }
+  end
+
+  describe file('/etc/nginx/conf.d/events/multi_accept.conf') do
+    it { should be_file }
+    it { should contain('multi_accept on') }
   end
 end
