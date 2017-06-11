d12bc03e2c1a717ac8e828b1b311f0121c6dbd09
Marcin Wolny <marcin@wutanic.com>
2017-06-11 20:07:46 +0200

    Change the default package name

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['nginx']['package'] = 'nginx'
+default['nginx']['package'] = 'nginx-full'
 default['nginx']['dir'] = '/etc/nginx'
 default['nginx']['modules_dir'] = '/usr/share/nginx/modules-available'
 default['nginx']['modules'] = []
